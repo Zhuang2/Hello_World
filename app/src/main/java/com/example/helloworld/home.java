58e36e28c28a75fadbb9597f311ffa886c076b8a
庄曦 <2939898974@qq.com>
2020-05-29 23:36:22 +0800

    实现功能：将用户资料显示到home界面

--- app/src/main/java/com/example/helloworld/home.java
+++ app/src/main/java/com/example/helloworld/home.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.helloworld.databinding.ActivityHomeBinding;
@@ -19,6 +20,15 @@ public class home extends AppCompatActivity {
 
         Intent intent = getIntent();//获取传过来的Intent对象
         String phone = intent.getStringExtra("data_phone");//取出对应的值
-        mBinding.textPhone.setText(phone);
+        mBinding.userPhone.setText(phone);
+
+        SharedPreferences sp = getSharedPreferences("user_info",MODE_PRIVATE);
+        String userName = sp.getString("name_"+phone,"0");
+        String userSex = sp.getString("sex_"+phone,"0");
+        String userSms = sp.getString("sms_"+phone,"0").equals("")?"接受":"拒绝";
+        mBinding.userName.setText(userName);
+        mBinding.userSex.setText(userSex);
+        String temp = mBinding.userSms.getText().toString()+":"+userSms;//接受信息推送：接送/拒绝
+        mBinding.userSms.setText(temp);
     }
 }
