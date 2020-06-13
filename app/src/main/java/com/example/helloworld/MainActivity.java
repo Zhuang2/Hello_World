91679b9dff868ae0b47b7cf08881c1cbc69a984c
庄曦 <2939898974@qq.com>
2020-06-13 14:31:52 +0800

    实现功能:点击忘记密码跳转至RegisterActivity

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -68,6 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mBinding.textForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ResetPwdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
