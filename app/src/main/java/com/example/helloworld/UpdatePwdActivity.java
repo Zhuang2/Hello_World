1b56abff587eb8b0fa63b9fbde7b18e584854275
庄曦 <2939898974@qq.com>
2020-06-13 14:35:29 +0800

    实现功能：修改密码和回调登录界面

--- app/src/main/java/com/example/helloworld/UpdatePwdActivity.java
+++ app/src/main/java/com/example/helloworld/UpdatePwdActivity.java
@@ -2,13 +2,50 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.helloworld.databinding.ActivityUpdatePwdBinding;
 
 public class UpdatePwdActivity extends AppCompatActivity {
+
+    private ActivityUpdatePwdBinding mBinding;
+    String pwd,pwdOk,phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_pwd);
+        mBinding = ActivityUpdatePwdBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pwd = mBinding.updatePwd.getText().toString().trim();
+                pwdOk = mBinding.updatePwdOk.getText().toString().trim();
+                String toast;
+                if(pwd.length()!=6){
+                    toast="密码长度必须为6位数";
+                }
+                else if(!pwd.equals(pwdOk)){
+                    toast="请确保密码是否一致";
+                }else{
+                    SharedPreferences sp = getSharedPreferences("user_info",MODE_PRIVATE);
+                    phone = getIntent().getStringExtra("phone");
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("pwd_"+phone,pwd);
+                    editor.putString("pwdOk_"+phone,pwdOk);
+                    editor.apply();
+                    toast="密码修改成功！";
+                    Intent intent = new Intent(UpdatePwdActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                Toast.makeText(UpdatePwdActivity.this,toast,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
